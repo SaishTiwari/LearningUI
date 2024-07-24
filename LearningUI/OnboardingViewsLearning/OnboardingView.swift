07cdd079b1a61918d6c5daada99e4e268dab35e3
SaishTiwari <tiwarisaish381@gmail.com>
2024-07-24 08:17:26 +0545

    Chautari

--- LearningUI/OnboardingViewsLearning/OnboardingView.swift
+++ LearningUI/OnboardingViewsLearning/OnboardingView.swift
@@ -17,10 +17,15 @@ struct OnboardingView: View {
      3 - Gender
      
      */
-    @State var onboardingstate : Int = 0
+    @State var onboardingstate : Int = 3
     @State var fullName : String = ""
     @State var age : Double = 18
     @State var gender :String =  ""
+    @State var showAlert : Bool = false
+    @State var showAlert2 : Bool = false
+
+    @State var alertTitle : String = ""
+    let transition : AnyTransition = .asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading))
     var body: some View {
         ZStack{
             Spacer()
@@ -28,15 +33,21 @@ struct OnboardingView: View {
                 switch onboardingstate{
                 case 0:
                     welcomeScreen
+                        .transition(transition)
                     
                 case 1:
                     namesection
+                        .transition(transition)
+
                     
                 case 2:
                     agesection
+                        .transition(transition)
+
                     
                 case 3:
-                    gendersection
+                    gendersection                        .transition(transition)
+
                     
                     
                 default:
@@ -49,6 +60,10 @@ struct OnboardingView: View {
                 
                 Spacer()
                 Spacer()
+                Spacer()
+                
+                loginRedirectory
+                Spacer()
                 bottombutton
                 
                 
@@ -59,6 +74,13 @@ struct OnboardingView: View {
             
             
         }
+        .alert(isPresented: $showAlert, content: {
+            return Alert(title: Text(alertTitle), dismissButton: .cancel())
+        })
+        
+        .alert(isPresented: $showAlert2, content: {
+            return Alert(title: Text(alertTitle), dismissButton: .cancel(Text("Confirm")))
+        })
     }
 }
 
@@ -67,8 +89,26 @@ struct OnboardingView: View {
 }
 
 extension OnboardingView{
+    
+    
+    private var loginRedirectory : some View{
+        
+        VStack(spacing: -15) {
+            
+            
+            
+        }
+        .padding(5)
+    }
+
+               
+            
+                    
+        
+        
+    
     private var bottombutton : some View{
-        Text("Sign in ")
+       Text (onboardingstate == 0 ? "SIGN UP" : onboardingstate == 3 ? "FINISH" : "Next")
             .foregroundColor(.purple)
             .frame(maxWidth:.infinity )
             .frame(height: 55)
@@ -77,22 +117,15 @@ extension OnboardingView{
             .cornerRadius(10)
             .padding()
             .onTapGesture {
-                
+                handleNextButton()
             }
         
     }
     private var welcomeScreen : some View{
-        VStack(spacing:40){
+        VStack(spacing:5){
             Spacer()
-            //  Text("Hello")
-            Image(systemName: "heart.text.square.fill")
-                
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .foregroundColor(.white)
+            Spacer()
             
-                
             
             Text("चौतारी: प्रेमको पहिलो कदम").foregroundColor(.white)
                 .font(.largeTitle).fontWeight(.semibold)
@@ -100,8 +133,10 @@ extension OnboardingView{
                     .frame(height: 3)
                     .offset(y:20)
                     .foregroundColor(.white)
-                
+                         
                 )
+            
+            
             
             Text("Nepal's #1 dating app, where finding love comes with a smile!")
                 .padding(.horizontal)
@@ -110,8 +145,68 @@ extension OnboardingView{
                 .multilineTextAlignment(.center)
                 .foregroundColor(.white)
             
+            
+            
+            Spacer()
+            
+            //  Text("Hello")
+            Image(systemName: "tree.fill")
+            
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 175)
+                .foregroundColor(.green)
+            
+                .padding()
+            
+                Spacer()
+            
+            VStack(spacing:-20) {
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .padding()
+                    .overlay(HStack{
+                        Image(systemName: "apple.logo").resizable().scaledToFit() .foregroundColor(.white).frame(width: 20)
+                        Text("Continue with Apple").foregroundColor(.purple)
+                        
+                        
+                    })
+                
+                
+                
+                RoundedRectangle(cornerRadius: 20).foregroundColor(.blue)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .padding()
+                    .overlay(HStack{
+                        Image(systemName: "f.circle.fill").resizable().scaledToFit().foregroundColor(.white).frame(width: 20)
+                        Text("Continue with Facebook").foregroundColor(.white)
+                        
+                        
+                    })
+                
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .padding()
+                    .overlay(HStack{
+                        Image(systemName: "phone.circle.fill").resizable().scaledToFit().foregroundColor(.white).frame(width: 20)
+                        Text("Use cell phone number").foregroundColor(.purple)
+                        
+                    })
+            }
+        
+
+            
+            
             Spacer()
             Spacer()
+            
+            
+            
+            
             
                 
             
@@ -130,12 +225,15 @@ extension OnboardingView{
                 .foregroundColor(.white)
             
             TextField("Enter your name", text: $fullName)
+            
                 .font(.headline)
                 .frame(width: 350, height: 50)
             
-                .foregroundColor(.black)
+                //.foregroundColor(.gray)
                 .padding(.horizontal,10)
-                .background(Color.white).cornerRadius(10)
+                .background(Color.white
+                    ).cornerRadius(10)
+                .foregroundColor(.black)
                 
             
             Spacer()
@@ -184,18 +282,22 @@ extension OnboardingView{
             
             Spacer()
         
-            Text(gender.count>1 ? gender :"Select a gender")
+            Text("What's your gender?")
                 .foregroundColor(.white)
                 .font(.largeTitle)
                 .fontWeight(.semibold)
             
-            Picker("Gender", selection: $gender) {
+            Picker(selection: $gender) {
                 Text("Male").tag(1)
                 Text("Female").tag(2)
                 Text("Non Binary").tag(3)
+            } label: {
+                Text( gender.count>1 ? gender : "Choose your gender")
             }
-            .pickerStyle(InlinePickerStyle())
+
+               
             
+            .pickerStyle(MenuPickerStyle())
                 
               
             
@@ -206,4 +308,52 @@ extension OnboardingView{
         
     }
     }
+}
+
+//Functions:
+extension OnboardingView{
+    
+    func handleNextButton(){
+        
+        switch onboardingstate{
+        case 1:
+            
+            guard fullName.count > 3 else{
+                alertTitle = "Your name must be atleast three characters long"
+                showAlert.toggle()
+                return
+            }
+            
+     
+            
+            
+        case 3:
+            
+            guard gender.count > 1 else{
+                alertTitle = "You must select a gender"
+                showAlert.toggle()
+                return
+            }
+            
+            
+            
+            
+            
+        default :
+            break
+        
+        }
+        
+        if onboardingstate == 3{
+           //code
+        }
+        else{
+            
+        
+        withAnimation(.spring()){
+            onboardingstate += 1
+        }
+    }
+    }
+    
 }
